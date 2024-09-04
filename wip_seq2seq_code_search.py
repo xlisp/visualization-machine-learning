85ad93e4463c94fe644851b4e82850b090983250
stevechan <chanshunli@gmail.com>
2024-09-04 14:31:14 +0800

    _

--- wip_seq2seq_code_search.py
+++ wip_seq2seq_code_search.py
@@ -53,7 +53,7 @@ import torch
 import torch.nn as nn
 
 class Encoder(nn.Module):
-def __init__(self, input_dim, emb_dim, hidden_dim, n_layers, dropout):
+    def __init__(self, input_dim, emb_dim, hidden_dim, n_layers, dropout):
         super().__init__()
         self.embedding = nn.Embedding(input_dim, emb_dim)
         self.rnn = nn.LSTM(emb_dim, hidden_dim, n_layers, dropout=dropout)
