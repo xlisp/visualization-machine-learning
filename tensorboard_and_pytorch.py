df3324649c38b98a52da8d279aae89a4817f53d7
stevechan <chanshunli@gmail.com>
2024-09-08 11:52:56 +0800

    Add tensorboard_and_pytorch.py https://pytorch.org/tutorials/recipes/recipes/tensorboard_with_pytorch.html

---
+++ tensorboard_and_pytorch.py
@@ -0,0 +1,28 @@
+## https://pytorch.org/tutorials/recipes/recipes/tensorboard_with_pytorch.html
+import torch
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter()
+
+x = torch.arange(-5, 5, 0.1).view(-1, 1)
+y = -5 * x + 0.1 * torch.randn(x.size())
+
+model = torch.nn.Linear(1, 1)
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.1)
+
+def train_model(iter):
+    for epoch in range(iter):
+        y1 = model(x)
+        loss = criterion(y1, y)
+        writer.add_scalar("Loss/train", loss, epoch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+train_model(10)
+writer.flush()
+
+writer.close()
+
+## tensorboard --logdir=runs
+
