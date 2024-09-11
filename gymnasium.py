ef5b68cd3d0a0ce6f434d642c2a19cfe8857a2df
stevechan <chanshunli@gmail.com>
2024-09-11 14:09:11 +0800

    RL games: https://gymnasium.farama.org/ it is can run ui

---
+++ gymnasium.py
@@ -0,0 +1,14 @@
+# RL games: https://gymnasium.farama.org/ it's can run ui
+
+import gymnasium as gym
+env = gym.make("LunarLander-v2", render_mode="human")
+observation, info = env.reset(seed=42)
+for _ in range(1000):
+   action = env.action_space.sample()  # this is where you would insert your policy
+   observation, reward, terminated, truncated, info = env.step(action)
+
+   if terminated or truncated:
+      observation, info = env.reset()
+
+env.close()
+
